7ad468aef4ef2d47b24007c60e1e58e6660cf186
ABHIRUP <abhirupsaha02@gmail.com>
2023-12-20 15:58:48 +0530

    auth done

--- router/auth.route.js
+++ router/auth.route.js
@@ -5,11 +5,11 @@ import { registerMail } from "../utils/mail.js"
 const router = Router()
 
 
-router.route('/add').get(authController.addEducator)
-router.route('/login').get(authController.loginEducator)
+router.route('/add').post(authController.addEducator)
+router.route('/login').post(authController.loginEducator)
 router.route('/profile/:id').get(authController.getProfileById)
-router.route('/update/:id').get(authController.getProfileByIdAndUpdate)
-router.route('/delete/:id').get(authController.deleteProfileById)
+router.route('/update/:id').patch(authController.getProfileByIdAndUpdate)
+router.route('/delete/:id').delete(authController.deleteProfileById)
 router.route('/mail').post(registerMail)
 
 
