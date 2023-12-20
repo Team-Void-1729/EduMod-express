356a0c6088adf533fb9d981da1e245e3c0350d29
ABHIRUP <abhirupsaha02@gmail.com>
2023-12-20 15:56:22 +0530

    course done

--- controllers/courses.controller.js
+++ controllers/courses.controller.js
@@ -23,3 +23,36 @@ export async function addCourse(req, res, next) {
         return res.status(500).json(err)
     }
 }
+
+export async function getCourse(req, res, next) {
+    try {
+        const { data } = await courseInstance.get(`/details/${req.params.title}`)
+
+        return res.status(200).json(data)
+    }
+    catch (err) {
+        return res.status(500).json(err)
+    }
+}
+
+export async function updateCourse(req, res, next) {
+    try {
+        const { data } = await courseInstance.patch(`/update/${req.params.id}`, req.body)
+
+        return res.status(200).json(data)
+    }
+    catch (err) {
+        return res.status(500).json(err)
+    }
+}
+
+export async function deleteCourse(req, res, next) {
+    try {
+        const { data } = await courseInstance.delete(`/delete/${req.params.id}`)
+
+        return res.status(200).json(data)
+    }
+    catch (err) {
+        return res.status(500).json(err)
+    }
+}
--- router/courses.route.js
+++ router/courses.route.js
@@ -7,8 +7,11 @@ import { ROLES } from "../entities/role.js"
 const router = Router()
 
 
+router.route('/add').get(Auth, restrict(ROLES.Developer, ROLES.Educator), courseController.addCourse)
+router.route('/update/:id').get(Auth, restrict(ROLES.Developer, ROLES.Educator), courseController.updateCourse)
+router.route('/delete/:id').get(Auth, restrict(ROLES.Developer, ROLES.Educator), courseController.deleteCourse)
 router.route('/details').get(courseController.getAllCourses)
-router.route('/add').post(Auth, restrict(ROLES.Developer, ROLES.Educator), courseController.addCourse)
+router.route('/details/:title').get(courseController.getCourse)
 
 
 export { router as CourseRouter }
